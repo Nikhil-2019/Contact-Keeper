94220430a8f8c195729c8b4d452b52a67113c0fc
nikhil <nikhil13012000@gmail.com>
2020-03-31 13:56:13 +0530

    deploying

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const app = express();
-
+const path = require('path');
 //Connecting database
 connectDB();
 
@@ -14,6 +14,16 @@ app.get('/', (req, res) => res.send({ msg: 'Welcome to the contacts keeper API' 
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
+
+//Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+	//Set static folder
+	app.use(express.static('client/build'));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 
 const PORT = process.env.PORT || 5000;
 
